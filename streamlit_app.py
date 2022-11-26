17d3f6a078f780f668533e5cb0bf15ee35bfe080
Jason Stewart <jason.stewart@gmail.com>
2022-11-26 18:03:56 -0500

    Update streamlit_app.py
    added icons for b/f menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit
 
 streamlit.title( 'My parents new healthy diner!' )
 
-streamlit.header( 'Breakfast menu' )
-streamlit.text( 'Omega 3 & Blueberry Oatmeal' )
-streamlit.text( 'Kale, Spinach and Rocket Smoothie' )
-streamlit.text( 'Hard-boiled free-range egg' )
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(🥑🍞' Avocado toast')
+
+   
