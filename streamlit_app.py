e4deafe0634971a04499e8e1ec2d997a0ac79594
Jason Stewart <jason.stewart@gmail.com>
2022-11-30 21:38:34 -0500

    bug fix: pandas alias name

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,6 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi" )
 
 # converts to data frame for display
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json()) 
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
